cf72e7795069811712c9340382cf29d8d1275d36
Alberto Calvo <alberto@arima.eu>
2020-10-09 12:24:58 +0200

    Minimal reproduction for dup i18n strings

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import '@angular/localize/init';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ng-xi18n-dup';
+  title = $localize`Some text`;
 }
